bec7583c781bda78cec5c8e7c14f75f0b2a6ad38
StanShishmanov <slaaff@protonmail.com>
2020-12-11 16:48:36 +0200

    Add files via upload

---
+++ Python Advanced - SoftUni/Advanced Functions/1. Multiply.py
@@ -0,0 +1,7 @@
+def multiply(*args):
+    result = 1
+
+    for x in args:
+        result *= x
+
+    return result
---
+++ Python Advanced - SoftUni/Advanced Functions/10. Min Max and Sum.py
@@ -0,0 +1,4 @@
+nums_input = [int(i) for i in input().split()]
+print(f'The minimum number is {min(nums_input)}')
+print(f'The maximum number is {max(nums_input)}')
+print(f'The sum number is: {sum(nums_input)}')
